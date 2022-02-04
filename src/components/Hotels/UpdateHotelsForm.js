9dd476f07d108f73324012cf2f2acd578316ec52
S.M.ABTAHI NOOR <abtahinoorkabid@gmail.com>
2022-02-04 08:56:56 +0600

    Update Page Done

---
+++ src/components/Hotels/UpdateHotelsForm.js
@@ -0,0 +1,45 @@
+import { Grid } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react';
+import FormButton from '../FormButton';
+import InputField from '../InputField';
+import TextArea from '../TextArea';
+
+function UpdateHotelsForm(props) {
+    return (
+        <form>
+
+            {/* hotel name and code  */}
+            <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 12, lg: 12 }} sx={{ marginBottom: 3 }}>
+                <Grid item xs={4} sm={8} md={12} lg={12}>
+                    <InputField label="Hotel Name" />
+                </Grid>
+
+            </Grid>
+
+            {/* hotel address   */}
+            <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 12, lg: 12 }} sx={{ marginBottom: 3 }}>
+                <Grid item xs={4} sm={8} md={12} lg={12}>
+                    <TextArea label="Hotel Address" />
+                </Grid>
+            </Grid>
+
+            {/* hotel telephone and button */}
+            <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 12, lg: 12 }} sx={{ marginBottom: 3 }}>
+                <Grid item xs={4} sm={8} md={6} lg={6}>
+                    <InputField label="Hotel Telephone" />
+                </Grid>
+                <Grid item xs={4} sm={8} md={6} lg={6}>
+                    <InputField label="Hotel Code" />
+                </Grid>
+            </Grid>
+
+            {/* buttons  */}
+            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'flex-end', height: '100%' }} >
+                <FormButton text="Update Hotel" />
+            </Box>
+        </form>
+    );
+}
+
+export default UpdateHotelsForm;
--- src/pages/Hotels/UpdateHotelPage.js
+++ src/pages/Hotels/UpdateHotelPage.js
@@ -1,11 +1,25 @@
+import { Box } from '@mui/material';
 import React from 'react';
+import UpdateHotelsForm from '../../components/Hotels/UpdateHotelsForm';
+import PageHeading from '../../components/PageHeading';
 
 function UpdateHotelPage(props) {
     return (
-        <div>
-            <h1>Update Hotels</h1>
+        <Box sx={{ padding: '30px 15px' }}>
 
-        </div>
+            {/* page heading  */}
+            <PageHeading text="Update Hotel" />
+            {/* form  */}
+            <Box sx={{
+                background: '#fff',
+                borderRadius: '10px',
+                padding: '25px',
+                boxSizing: 'borderBox',
+                margin: '30px 0px'
+            }}>
+                <UpdateHotelsForm />
+            </Box>
+        </Box>
     );
 }
 
